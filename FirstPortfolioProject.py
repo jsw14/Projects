d83de5922b51896be75738619153d89f6f20ec99
James Wilson <wilsonjamess2799@gmail.com>
2022-01-30 23:32:54 -0500

    Initial instructions

---
+++ FirstPortfolioProject.py
@@ -0,0 +1,15 @@
+#Blackjack terminal game
+#prints one dealer card and prints two player cards
+#input stay or hit
+#print dealer cards on stay
+
+print("""
+Hello there! You are about to play Blackjack. The rules are simple: you are playing against the dealer. 
+The goal is to have a sum of card values closest to 21 but not over or else you bust and automatically lose that turn. 
+Each turn you and the dealer are given two cards, but one of the dealer's cards is hidden until you finish your actions for that turn.
+Based on the cards you have and the one dealer card shown, you can either decide to hit (get another card to add to your total this turn)
+or stay (stay with your current cards). You can hit as many times as you want until you bust (have a sum of over 21) or choose to stay.
+Once you stay, it is the dealers turn to reveal the hidden card and draw cards until they have a total of 17 or higher. 
+If the dealer busts, you win. Otherwise, who ever has the largest total wins. 
+"""
+)
